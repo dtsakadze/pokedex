585ecbbb60c78df6c347058ad3e7fc9c5fc8a213
Davit Tsakadze <davittsakadzee@gmail.com>
2025-01-23 20:53:19 +0400

    created simple scanner

--- repl.go
+++ repl.go
@@ -1,6 +1,29 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
