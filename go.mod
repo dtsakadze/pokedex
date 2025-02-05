2aaa9e2fa68064213ec6b7ad76cc3d1f41087631
Davit Tsakadze <davittsakadzee@gmail.com>
2025-02-06 01:09:25 +0400

    implemented the poke api

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/dtsakadze/pokedex.git
+module github.com/dtsakadze/pokedex
 
 go 1.23.5
--- repl.go
+++ repl.go
@@ -5,9 +5,17 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/dtsakadze/pokedex/internal/pokeapi"
 )
 
-func startRepl() {
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
+
+func startRepl(cfg *config) {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -23,7 +31,7 @@ func startRepl() {
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback()
+			err := command.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -45,7 +53,7 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name string
 	description string
-	callback func() error
+	callback func(*config) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -59,6 +67,16 @@ func getCommands() map[string]cliCommand {
 			name: "exit",
 			description: "Exit the Pokedex",
 			callback: commandExit,
+		},
+		"map": {
+			name: "map",
+			description: "Display the next location areas",
+			callback: commandMapf,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Display the previous location areas",
+			callback: commandMapb,
 		},
 	}
 }
