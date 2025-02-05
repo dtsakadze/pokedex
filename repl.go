1681210f0f8b1c074be3a3a45ca91b3a150e1836
Davit Tsakadze <davittsakadzee@gmail.com>
2025-02-06 01:19:52 +0400

    added the explore command

--- repl.go
+++ repl.go
@@ -28,10 +28,14 @@ func startRepl(cfg *config) {
 		}
 
 		commandName := words[0]
+		args := []string{}
+		if len(words) > 1 {
+			args = words[1:]
+		}
 
 		command, exists := getCommands()[commandName]
 		if exists {
-			err := command.callback(cfg)
+			err := command.callback(cfg, args...)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -53,7 +57,7 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name string
 	description string
-	callback func(*config) error
+	callback    func(*config, ...string) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -77,6 +81,11 @@ func getCommands() map[string]cliCommand {
 			name: "mapb",
 			description: "Display the previous location areas",
 			callback: commandMapb,
+		},
+		"explore": {
+			name:        "explore <location_name>",
+			description: "Explore a location",
+			callback:    commandExplore,
 		},
 	}
 }
